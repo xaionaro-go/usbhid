karelbilek/libusb_plus_hidapi

Changing package name

hid.go
@@ -8,7 +8,7 @@
 
 // +build linux,cgo darwin,!ios,cgo windows,cgo
 
-package hid
+package usb_hid
 
 /*
 #cgo CFLAGS: -I./hidapi/hidapi

libusb.go
@@ -7,7 +7,7 @@ Golang wrapper for libusb-1.0
 //-----------------------------------------------------------------------------
 
 // Package libusb provides go wrappers for libusb-1.0
-package libusb
+package usb_hid
 
 /*
 #cgo pkg-config: libusb-1.0

wchar.go
@@ -9,7 +9,7 @@
 // +build !ios
 // +build linux darwin windows
 
-package hid
+package usb_hid
 
 /*
 #include <wchar.h>

xaionaro-go/usbhid

hid.go
@@ -8,7 +8,7 @@
 
 // +build linux,cgo darwin,!ios,cgo windows,cgo
 
-package hid
+package usbhid
 
 /*
 #cgo CFLAGS: -I./hidapi/hidapi

libusb.go
@@ -7,7 +7,7 @@ Golang wrapper for libusb-1.0
 //-----------------------------------------------------------------------------
 
 // Package libusb provides go wrappers for libusb-1.0
-package libusb
+package usbhid
 
 /*
 #cgo pkg-config: libusb-1.0

wchar.go
@@ -9,7 +9,7 @@
 // +build !ios
 // +build linux darwin windows
 
-package hid
+package usbhid
 
 /*
 #include <wchar.h>